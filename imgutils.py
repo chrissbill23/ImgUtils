23fa4fb930efc601cd0f1b6b7ee843205718b0f5
chrissbill23 <christian2tv@gmail.com>
2020-09-16 23:41:04 +0200

    added image loading with more options like parallelism and resize

--- imgutils.py
+++ imgutils.py
@@ -5,26 +5,52 @@ from tqdm import tqdm
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import LabelBinarizer
+from skimage import transform,io
+import multiprocessing as mp
 
-def imgdataset(pathmaindir : str, codlabels='ohe', encoder=None):
+def collect_files(filenames,dirname, gray=False, size=None):
+    Xtmp = []
+    Ytmp = []
+    for filename in filenames:
+        Ytmp.append(os.path.basename(dirname))
+        img = io.imread(os.path.join(dirname, filename), as_gray=gray)
+        if size is not None:
+            img = transform.resize(img, size)
+        Xtmp.append(img)
+    return (Xtmp, Ytmp)
+def loadimgdataset(pathmaindir : str, codlabels='ohe', gray=False, size=None, parallel=False):
+    encoder = None
     X = []
     Y = []
-    for dirname, _, filenames in os.walk(pathmaindir):
-        for filename in tqdm(filenames):
-            Y.append(os.path.basename(dirname))
-            X.append(imageio.imread(os.path.join(dirname, filename)))
+    tot_works = mp.cpu_count()
+    if parallel == False:
+        for dirname, _, filenames in tqdm(os.walk(pathmaindir)):
+            ris = collect_files(filenames, dirname, gray, size)
+            Y += ris[1]
+            X += ris[0]
+    else:
+        with mp.Pool(processes=tot_works) as pool:
+            count = 1
+            res = []
+            for dirname, _, filenames in os.walk(pathmaindir):
+                res.append(pool.apply_async(collect_files, args=(filenames, dirname, gray, size)))
+                count += 1
+            for r in res:
+                res2 = r.get(timeout=800)
+                X += res2[0]
+                Y += res2[1]
     X = np.array(X)
     if X.shape[0] > 0:
         if codlabels == 'binary' or codlabels == 'integer':
-           encoder = LabelEncoder()
-           encoder.fit(Y)
+            encoder = LabelEncoder()
+            encoder.fit(Y)
         else:
             if codlabels == 'ohe':
-               encoder = LabelBinarizer()
-               encoder.fit(Y)
+                encoder = LabelBinarizer()
+                encoder.fit(Y)
         Y = encoder.transform(Y)
     return X, Y, encoder
-    
+'''
 def imgdatasetreg(pathmaindir : str, substrlabels: list, codlabels='ohe', encoder=None):
     X = []
     Y = []
@@ -49,3 +75,4 @@ def imgdatasetreg(pathmaindir : str, substrlabels: list, codlabels='ohe', encode
                encoder.fit(Y)
         Y = encoder.transform(Y)
     return X, Y, encoder
+'''
--- test.py
+++ test.py
@@ -1,6 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
 
-import imgutils as im
+# In[2]:
 
-#X, Y,enc = im.imgdataset("./Data/Test")
-X2, Y2, enc2 = im.imgdatasetreg("./chest_xray/train", ['bacteria', 'virus'], 'binary')
-print(Y2)
+
+from imgutils import *
+
+
+# # Load Dataset
+
+# ## Paralellized
+
+# ### RGB
+
+# No resize
+
+# In[2]:
+
+
+get_ipython().run_line_magic('time', 'X, Y,enc = loadimgdataset("./Data/Training",parallel=True)')
+del X
+del Y
+
+
+# Resize
+
+# In[3]:
+
+
+get_ipython().run_line_magic('time', 'X, Y,enc = loadimgdataset("./Data/Training",size=(64,64,3),parallel=True)')
+X.shape
+del X
+del Y
+
+
+# ### Gray
+
+# No resize
+
+# In[3]:
+
+
+get_ipython().run_line_magic('time', 'X, Y,enc = loadimgdataset("./Data/Training",gray=True,parallel=True)')
+del X
+del Y
+
+
+# Resize
+
+# In[4]:
+
+
+get_ipython().run_line_magic('time', 'X, Y,enc = loadimgdataset("./Data/Training",gray=True,size=(64,64),parallel=True)')
+X.shape
+del X
+del Y
+
+
+# ## Not Paralellized
+
+# ### RGB
+
+# No resize
+
+# In[5]:
+
+
+get_ipython().run_line_magic('time', 'X, Y,enc = loadimgdataset("./Data/Training")')
+del X
+del Y
+
+
+# Resize
+
+# In[6]:
+
+
+get_ipython().run_line_magic('time', 'X, Y,enc = loadimgdataset("./Data/Training",size=(64,64,3))')
+X.shape
+del X
+del Y
+
+
+# ### Gray
+
+# No resize
+
+# In[7]:
+
+
+get_ipython().run_line_magic('time', 'X, Y,enc = loadimgdataset("./Data/Training",gray=True)')
+del X
+del Y
+
+
+# Resize
+
+# In[8]:
+
+
+get_ipython().run_line_magic('time', 'X, Y,enc = loadimgdataset("./Data/Training",gray=True,size=(64,64))')
+X.shape
+del X
+del Y
+
+
+# In[ ]:
+
+
+
+
